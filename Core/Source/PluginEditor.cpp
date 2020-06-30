001fed7ecf30a55b06a071d1f5e17f3cbfe2752b
Haydon Cardew <>
2020-07-01 00:00:04 +0100

    First draft of the GUI done

--- Core/Source/PluginEditor.cpp
+++ Core/Source/PluginEditor.cpp
@@ -45,23 +45,23 @@ MainContentComponent::MainContentComponent()
     
     addAndMakeVisible(angleShown);
     
-    addAndMakeVisible(direction);
-    direction.setRange(0.0f, 3.1416f);
-    direction.onValueChange = [this] {
-        angleShown.changeAzimuth(direction.getValue()); 
-    };
-    direction.setSliderStyle(juce::Slider::LinearVertical);
-    direction.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0.0f, 0.0f);
-    
     addAndMakeVisible(spread);
-    spread.setSliderStyle(Slider::Rotary);
-    spread.setRange(0.0f, 3.1416f * 2.0f);
+    spread.setRange(0.0f, 3.1416f);
     spread.onValueChange = [this] {
-        listener.rotateBy(spread.getValue());
-        angleShown.changeCentrePosition(spread.getValue());
+        angleShown.changeAzimuth(spread.getValue()); 
     };
-    spread.setRotaryParameters(0.f, 6.282f, false);
+    spread.setSliderStyle(juce::Slider::LinearVertical);
     spread.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0.0f, 0.0f);
+    
+    addAndMakeVisible(direction);
+    direction.setSliderStyle(Slider::Rotary);
+    direction.setRange(0.0f, 3.1416f * 2.0f);
+    direction.onValueChange = [this] {
+        listener.rotateBy(direction.getValue());
+        angleShown.changeCentrePosition(direction.getValue());
+    };
+    direction.setRotaryParameters(0.f, 6.282f, false);
+    direction.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0.0f, 0.0f);
     
     addAndMakeVisible(orderSelect);
     addAndMakeVisible(listener);
@@ -71,11 +71,14 @@ MainContentComponent::MainContentComponent()
 
 void MainContentComponent::resized()
 {
-    listener.setBoundsRelative(0.27f, 0.16f, 0.2f, 0.2f);
-    angleShown.setBoundsRelative(0.27f, 0.16f, 0.8f, 0.8f);
-    direction.setBoundsRelative(0.14f, 0.3f, 0.2f, 0.6f);
-    spread.setBoundsRelative(0.1f, 0.1f, 0.3f, 0.3f);
-    orderSelect.setBoundsRelative(0.75f, 0.17f, 0.2f, 0.1f);
+    float listenerSize = 0.14764f;
+    listener.setBoundsRelative(0.452, 0.43927, listenerSize, listenerSize);
+    float angleShownSize = 0.762392f;
+    angleShown.setBoundsRelative(0.278609, 0.120073, angleShownSize, angleShownSize);
+    
+    direction.setBoundsRelative(0.0965741f, 0.0915974f, 0.155535f, 0.306973f);
+    spread.setBoundsRelative(0.137192f, 0.382995f, 0.3f, 0.52f);
+    orderSelect.setBoundsRelative(0.76f, 0.165f, 0.2, 0.1f);
 }
 
 void MainContentComponent::paint(Graphics& g)
